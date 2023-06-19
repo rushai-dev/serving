80e495475dedef98baeaf271dce83515be12ae01
RUSH.AI <ratchaphonboss@gmail.com>
2023-06-19 22:00:34 +0700

    Create setup.py

---
+++ src/setup.py
@@ -0,0 +1,21 @@
+from itertools import chain
+from setuptools import find_packages, setup
+
+setup(
+    name="rush-serving",
+    author="RUSH.AI",
+    author_email="ratchaphonboss@gmail.com",
+    version="0.1",
+    description="serving api wrapper",
+    url="https://github.com/rushai-dev/serving",
+    license="GPL",
+    license_files=["LICENSE"],
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.6",
+    install_requires=[
+        "uvicorn<=0.20.0",
+        "fastapi<=0.92.0"
+    ],
+    zip_safe=False,
+)
